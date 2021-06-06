1ac6f0644f4bc76d3fb7dc5a513eb104c85da8c4
kauanabrahao <kauanabrahao@gmail.com>
2021-06-06 13:54:50 -0300

    script tabela

---
+++ src/dao/visitante_cad.sql
@@ -0,0 +1,3 @@
+CREATE TABLE visitante_cad (
+cpf varchar(12) PRIMARY KEY, rg varchar(12), nome varchar(50), email varchar(20), telefone varchar(20),
+login varchar(20) NOT NULL, senha varchar(20) NOT NULL, datanasc DATE);
